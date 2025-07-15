492a50463935149c1de85ea66332802841ac41b4
Vladyslav <bluues1536@gmail.com>
2025-07-15 12:30:05 +0100

    Initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import { Header } from "@/widgets/header";
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <h1 className="font-PTSerif">Hello from App</h1>
+    </div>
+  );
+}
